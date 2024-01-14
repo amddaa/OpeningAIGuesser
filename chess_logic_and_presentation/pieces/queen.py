a1063a83d8653820ed9aebed9d00489d755e7ac5
amddaa <slusarekadam1@gmail.com>
2024-01-14 23:28:51 +0100

    Fix type hinting

--- chess_logic_and_presentation/pieces/queen.py
+++ chess_logic_and_presentation/pieces/queen.py
@@ -15,8 +15,8 @@ class Queen(Piece):
         pieces_arr: list[Queen],
         move_to: str,
         ambiguity_help: str | None,
-        pieces_white: list[list[type[Piece]]],
-        pieces_black: list[list[type[Piece]]],
+        pieces_white: list[list],
+        pieces_black: list[list],
     ) -> str | None:
         from chess_logic_and_presentation.chess_board import Board
 
